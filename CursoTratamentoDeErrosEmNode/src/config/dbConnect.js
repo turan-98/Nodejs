f583521205b9c1bb17e813f2a67f3af23fd7e753
turan-98 <santanasamuel08@gmail.com>
2024-07-02 19:19:38 -0300

    🚑 security issue

--- CursoTratamentoDeErrosEmNode/src/config/dbConnect.js
+++ CursoTratamentoDeErrosEmNode/src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 const connect = process.env.DB_CONNECTION_STRING
-mongoose.connect(`mongodb+srv://santanasamuel08:NrqVkuLomkE9VMIe@cluster0.r29uumt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+mongoose.connect(`mongodb+srv://santanasamuel08:''@cluster0.r29uumt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
 
 let db = mongoose.connection;
 
