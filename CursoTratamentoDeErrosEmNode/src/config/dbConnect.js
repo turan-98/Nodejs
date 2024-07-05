6c96057eafe550d6e4b3478de7faafcc0cbeac35
turan-98 <santanasamuel08@gmail.com>
2024-07-05 17:43:02 -0300

    🔒️ fix security issues

--- CursoTratamentoDeErrosEmNode/src/config/dbConnect.js
+++ CursoTratamentoDeErrosEmNode/src/config/dbConnect.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
-const connect = process.env.DB_CONNECTION_STRING
-mongoose.connect(`mongodb+srv://santanasamuel08:''@cluster0.r29uumt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 let db = mongoose.connection;
 
 export default db;
-
